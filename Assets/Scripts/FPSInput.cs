3c57c7f3ac79754f4372630d4e6c46edcf2a22b9
Bryan Takemoto <35548639+AkagiCoder@users.noreply.github.com>
2020-09-29 00:50:24 -0700

    Polished the movement and mouse look script

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))] // Make CharacterController component mandatory for this script to work
+[AddComponentMenu("Control Script/FPS Input")]  // Adds this component to the 'Add Component' menu
 public class FPSInput : MonoBehaviour
 {
     // Variable that adjusts the movement speed
     public float speed = 6.0f;
+    // Variable to reference the Player's CharacterController component
+    private CharacterController _charController;
+    // Variable for gravity's acceleration
+    public float gravity = -9.8f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Get the CharacterController component that's attached to this object (Player)
+        _charController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -18,6 +25,15 @@ public class FPSInput : MonoBehaviour
     {
         float deltaX = Input.GetAxis("Horizontal") * speed;
         float deltaZ = Input.GetAxis("Vertical") * speed;
-        transform.Translate(deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime);
+
+        // Problem with using transform as movement is that the Player freely clips through the wall
+        //transform.Translate(deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime);
+
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed); // Limit the movement speed of this vector
+        movement.y = gravity;   // Apply gravity
+        movement *= Time.deltaTime; // Make the movement frame rate independent
+        movement = transform.TransformDirection(movement);  // Transform movement vector from local to global coordinates
+        _charController.Move(movement); // Use the Charactercontroller to move by that vector
     }
 }
--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -15,8 +15,8 @@ public class MouseLook : MonoBehaviour
     // Variable to select the axes
     public RotationAxes axes = RotationAxes.MouseXAndY;
     // Controls the speed of horizontal and vertical rotation
-    public float sensitivityHor = 9.0f;
-    public float sensitivityVert = 9.0f;
+    public float sensitivityHor = 2.0f;
+    public float sensitivityVert = 2.0f;
 
     public float minimumVert = -45.0f;
     public float maximumVert = 45.0f;
